9e168c0ee14796a025c6611170c4dd1f1d90e69a
X-RAY <intelekt200012@gmail.com>
2023-02-22 15:32:51 +0200

    fix

--- src/js/slick.js
+++ src/js/slick.js
@@ -7,22 +7,4 @@ $('.responsive').slick({
   slidesToScroll: 1,
   adaptiveHeight: true,
   slidesToShow: 3,
-
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ],
 });
