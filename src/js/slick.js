6f581addafe37565c81364166c9f12cb2e344e1f
X-RAY <intelekt200012@gmail.com>
2023-02-27 14:55:19 +0200

    save

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,7 +1,7 @@
 $('.responsive').slick({
   // dots: true,
-  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+  prevArrow: '<button type="button" class="my-slick-prev">&#60;</button>',
+  nextArrow: '<button type="button" class="my-slick-next">&#62;</button>',
   infinite: true,
   speed: 300,
   slidesToScroll: 1,
